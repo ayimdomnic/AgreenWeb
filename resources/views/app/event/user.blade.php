f908d71223d09a7eda49bd1128b790aeb9e63c33
Billy Berthod <billy.berthod@gmail.com>
2016-12-05 17:41:48 +0100

    Adding datepicker with good dates

--- resources/views/app/event/user.blade.php
+++ resources/views/app/event/user.blade.php
@@ -14,14 +14,89 @@
 			@endforeach
 		</select>
 
-		<input class="mdl-textfield__input" id="daterange" type="text" name="daterange"/>
+		<input class="mdl-textfield__input" id="demo" type="text" name="daterange"/>
 
 		<script type="text/javascript">
 			$( document ).ready(function() {
-				$(function() {
-					$('input[name="daterange"]').daterangepicker();
-				});
+				var currentDateFirst = moment().startOf('day').format("YYYY/MM/DD H:m:s");
+				var currentDateLast = moment().endOf('day').format("YYYY/MM/DD H:m:s");
+				var currentMonthFirst = moment().startOf('month').format("YYYY/MM/DD H:m:s");
+				var currentMonthLast = moment().endOf('month').format("YYYY/MM/DD H:m:s");
+				var lastWeekFirst = moment().subtract(1, 'weeks').startOf('isoWeek')
+				var lastWeekLast = moment().format("YYYY/MM/DD H:m:s");
+				var lastMonthFirst = moment().subtract(1, 'month').startOf('month')
+				var lastMonthEnd = moment().subtract(1, 'month').endOf('month')
+				var last30Days = moment().subtract(30, 'days');
+				var last7Days = moment().subtract(7, 'days');
+				var yesterdayFirst = moment().subtract(1, 'day');
+				var yesterdayLast = moment().subtract(1, 'day').endOf('day');
 
+				$('#demo').daterangepicker({
+					"timePicker": true,
+					"timePicker24Hour": true,
+					"ranges": {
+						"Aujourd'hui": [
+						currentDateFirst,
+						currentDateLast
+						],
+						"Hier": [
+						yesterdayFirst,
+						yesterdayLast
+						],
+						"7 derniers jours": [
+						last7Days,
+						currentDateLast
+						],
+						"30 derniers jours": [
+						last30Days,
+						currentDateLast
+						],
+						"Ce mois-ci": [
+						currentMonthFirst,
+						currentMonthLast
+						],
+						"Le mois dernier": [
+						lastMonthFirst,
+						lastMonthEnd
+						]
+					},
+					"locale": {
+						"format": "YYYY/MM/DD H:m:s",
+						"separator": " - ",
+						"applyLabel": "Apply",
+						"cancelLabel": "Cancel",
+						"fromLabel": "From",
+						"toLabel": "To",
+						"customRangeLabel": "Custom",
+						"weekLabel": "W",
+						"daysOfWeek": [
+						"Su",
+						"Mo",
+						"Tu",
+						"We",
+						"Th",
+						"Fr",
+						"Sa"
+						],
+						"monthNames": [
+						"Janvier",
+						"Fevrier",
+						"Mars",
+						"Avril",
+						"Mai",
+						"Juin",
+						"Juillet",
+						"Aout",
+						"Septembre",
+						"Octobre",
+						"Novembre",
+						"Decembre"
+						],
+						"firstDay": 1
+					},
+				}, function(start, end, label) {
+					console.log("New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')");
+				});
 				$(function(){
 					var currentDate = moment().format("DD-MM-YYYY");
 					document.getElementById("daterange").value = currentDate + " - " + currentDate;
