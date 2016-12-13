bfb3b07930b5b9685cb2478886cad016bbecba38
Billy Berthod <billy.berthod@gmail.com>
2016-12-13 11:39:29 +0100

    Add try and catch

--- public/webservices/put_all_ble_raspberry.php
+++ public/webservices/put_all_ble_raspberry.php
@@ -3,19 +3,23 @@ use App\Fitting;
 
 $data = json_decode(file_get_contents('php://input'), true);
 
-foreach ($data as $result) {
-    $fitting = new Fitting;
-    $fitting->Mac = $result["1"];
-    $fitting->idUser = $result["2"];
-    $fitting->type = 1;
-    $fitting->timesFitting = $result["3"];
-    $fitting->isSync = 0;
-    try {
-    $fitting->save();
-    } catch (Exception $e) {
-    	return $e;
-    }
+try {
+	$fitting->save();
+	foreach ($data as $result) {
+		$fitting = new Fitting;
+		$fitting->Mac = $result["1"];
+		$fitting->idUser = $result["2"];
+		$fitting->type = 1;
+		$fitting->timesFitting = $result["3"];
+		$fitting->isSync = 0;
+		try {
+			$fitting->save();
+		} catch (Exception $e) {
+			return $e;
+		}
+	}
+} catch (Exception $e) {
+	return $e;
 }
+
 ?>
-
-
