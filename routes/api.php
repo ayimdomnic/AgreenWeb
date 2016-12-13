01a21905e5a75a368a9360308bbea495e697f3ce
Billy Berthod <billy.berthod@gmail.com>
2016-12-13 14:11:07 +0100

    Test

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
 
-Route::post('/receiveEventsRaspberry', 'EventController@receiveEventsRaspberry');
+Route::post('receiveEventsRaspberry', 'EventController@receiveEventsRaspberry');
