9cf60d4fce2ad7f283c752f475e2c09320135b3a
Billy Berthod <billy.berthod@gmail.com>
2016-12-13 11:56:38 +0100

    Cleaning

--- public/webservices/put_all_ble_raspberry.php
+++ public/webservices/put_all_ble_raspberry.php
@@ -3,23 +3,13 @@ use App\Fitting;
 
 $data = json_decode(file_get_contents('php://input'), true);
 
-try {
+foreach ($data as $result) {
+	$fitting = new Fitting;
+	$fitting->Mac = $result["1"];
+	$fitting->idUser = $result["2"];
+	$fitting->type = 1;
+	$fitting->timesFitting = $result["3"];
+	$fitting->isSync = 0;
 	$fitting->save();
-	foreach ($data as $result) {
-		$fitting = new Fitting;
-		$fitting->Mac = $result["1"];
-		$fitting->idUser = $result["2"];
-		$fitting->type = 1;
-		$fitting->timesFitting = $result["3"];
-		$fitting->isSync = 0;
-		try {
-			$fitting->save();
-		} catch (Exception $e) {
-			return $e;
-		}
-	}
-} catch (Exception $e) {
-	return $e;
 }
-
 ?>
