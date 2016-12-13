6fda050049d525d2469b2bba40e104138ffd5609
Billy Berthod <billy.berthod@gmail.com>
2016-12-13 10:27:24 +0100

    Use event class for webservices

--- public/cron/put_all_event_raspberry_new.php
+++ public/cron/put_all_event_raspberry_new.php
@@ -2,7 +2,6 @@
 
 use App\Event;
 
-$event = new Event;
 
 $servername = "localhost";
 $dbname = "agreenweb";
@@ -21,13 +20,10 @@ $data = json_decode(file_get_contents('php://input'), true);
 $fileName = "9999111";
 
 foreach ($data as $result) {
-    $id = $result["0"];
-    $Lat = $result["1"];
-    $Lon = $result["2"];
+
     $fix_date = $result["3"];
     $fix_time = $result["4"];
-    $name = $result["6"];
-    $iduser = $result["7"];
+
     $fix_date_day = substr($fix_date, -6, 2);
     $fix_date_month = substr($fix_date, -4, 2);
     $fix_date_year = substr($fix_date, -2, 2);
@@ -39,31 +35,32 @@ foreach ($data as $result) {
     $dateGPS = $dateTimeGPS . " " . $heureGps;
     $txt = "" . $iduser . "-". $id . ";" . $name . "-" . $Lon .";" . $Lat .";" . $dateGPS .";0;0;0";
     $myfile = file_put_contents("".$fileName.".txt", $txt.PHP_EOL , FILE_APPEND);
-try
-{
-    $req = $cnx->prepare("INSERT INTO events (idUser, idApp, name, lon, lat, dateGPS, isInside, idParcelle, altitude, isSync) VALUES (:idUser, :id, :name, :lon, :lat, :dateGPS, :isInsideParcelle, :idParcelle, :altitude, :isSync)");
-    $req->execute(array(
-        "idUser" => $iduser,
-        "id" => $id,
-        "name" => $name,
-        "lon" => $Lon,
-        "lat" => $Lat,
-        "dateGPS" => $dateGPS,
-        "isInsideParcelle" => 0,
-        "idParcelle" => 0,
-        "altitude" => 0,
-        "isSync" => 0,
-        ));
-    $dateGPSSubstring = substr($dateGPS, 0, -9);
-    $fileName = "9999" . $dateGPSSubstring ."";
-    $txt = "9999;" .  $id .";RaspBerry" . $Lon .";" . $Lat .";" . $dateGPS .";0;0;0";
-    $myfile = file_put_contents("".$fileName.".txt", $txt.PHP_EOL , FILE_APPEND);
-}
-catch (PDOException $e)
-{
-    http_response_code(500);
-    echo $e;
-}
+    
+    $event = new Event;
+    $event->idApp = $result["0"];
+    $event->idUser = $result["7"];
+    $event->name = $result["6"];
+    $event->lon = $result["2"];
+    $event->lat = $result["1"];
+    $event->dateGps = $dateGPS;
+    $event->isInside = 0;
+    $event->idParcelle = 0;
+    $event->altitude = 0;
+    $event->isSync = 0;
+    $event->save();
+
+    try
+    {
+        $dateGPSSubstring = substr($dateGPS, 0, -9);
+        $fileName = "9999" . $dateGPSSubstring ."";
+        $txt = "9999;" .  $id .";RaspBerry" . $Lon .";" . $Lat .";" . $dateGPS .";0;0;0";
+        $myfile = file_put_contents("".$fileName.".txt", $txt.PHP_EOL , FILE_APPEND);
+    }
+    catch (PDOException $e)
+    {
+        http_response_code(500);
+        echo "Insert text file exception" . $e . "";
+    }
 }
 
 ?>
