fbc95c2f94c8881fc572119f36d26dc172e9381c
Billy Berthod <billy.berthod@gmail.com>
2016-12-13 10:11:20 +0100

    Add csrf execption

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,8 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
+        receiveEventsRaspberry/,
+        web/receiveEventsRaspberry/,
         receiveEventsRaspberry,
         /receiveEventsRaspberry
     ];
